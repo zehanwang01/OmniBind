274ec7ed0a50b57d075b467919b194186072330d
zhang-ziang <745656480@qq.com>
2024-06-19 21:59:02 +0800

    preparation update: installation of  knn_cuda and pointnet ops

--- preparation.sh
+++ preparation.sh
@@ -9,3 +9,14 @@ pip install .
 # install omnibind envs, overwrite imagebind envs
 cd ..
 pip install -r requirements.txt
+# install KNN_CUDA pytorch
+git clone https://github.com/unlimblue/KNN_CUDA.git
+cd KNN_CUDA
+make && make install
+cd ..
+# install pointnet2 ops
+pip install "git+git://github.com/erikwijmans/Pointnet2_PyTorch.git#egg=pointnet2_ops&subdirectory=pointnet2_ops_lib"
+# if use cuda12, use command below instead
+git clone https://github.com/HeBangYan/pointnet2_ops_CUDA12.0.git
+cd pointnet2_ops_CUDA12.0
+pip install pointnet2_ops_lib/.
